cd7fe479c4706159a4f2431614508e57922cd1a1
kashaffatima1 <arhamanwar000@gmail.com>
2024-08-11 03:48:12 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+ 
+let todos: any[] = [];
+let condition = true;
+
+    while (condition) {
+  let addTask = await inquirer.prompt
+  ([
+    {
+      type: 'input',
+      name: 'firstquestion', // Specify the 'name' property here
+      message: 'What would you like to add in your todos?',
+    },
+
+    {
+        type: `confirm`,
+        name: `secondquestion`, //specify the name property here
+        message: "would you like to add more in your todos?",
+        default: true,
+    }
+  ]    
+);
+todos.push(addTask.firstquestion);
+console.log(todos)
+condition = addTask.secondquestion
+
+
+};
